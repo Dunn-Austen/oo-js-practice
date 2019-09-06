49ac48e51641e66049c3a341708caec8daaa2f6b
Alan Turing <austeyd@gmail.com>
2019-09-06 15:21:29 -0600

    Initial commit

--- objects-and-functions/lib/student.js
+++ objects-and-functions/lib/student.js
@@ -1,1 +1,13 @@
+class Student {
+  constructor(object) {
+    this.name = object.name;
+    this.age = object.age;
+    this.scores = [];
+    this.avergeGrade = (number[1] * number[2]) / 2;
+  }
+  logScore(number) {
+    this.scores.push(number);
+  }
+}
 
+module.exports = Student;
